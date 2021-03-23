3480d02b3881adf42352188177a62dcd3874c5de
Tommy Barvåg <tommy@barvaag.com>
2021-03-24 00:04:56 +0100

    Remove header section from areas

--- src/pages/omraade.js
+++ src/pages/omraade.js
@@ -1,4 +1,3 @@
-import { Heading } from "@chakra-ui/layout";
 import * as React from "react";
 import Areas from "../components/areas";
 import ContactUs from "../components/contactUs";
@@ -6,7 +5,6 @@ import ContactUs from "../components/contactUs";
 export default function Area(props) {
   return (
     <>
-      <Heading textAlign="center">Bare ett forslag til hvordan man kan liste ut områder</Heading>
       <Areas />
       <ContactUs />
     </>
