45e057ef566645677da11c3d0ad50e3d2f31f807
Tommy Barvåg <tommy@barvaag.com>
2021-03-29 22:29:54 +0200

    Revert mail change

--- src/pages/api/sendMail.js
+++ src/pages/api/sendMail.js
@@ -21,7 +21,7 @@ export default async (req, res) => {
     sendgridMail.setApiKey(apiKey);
 
     await sendgridMail.send({
-      to: "gaute@hellandutleie.no" ?? "post@lagerbyggvest.no",
+      to: "post@lagerbyggvest.no",
       from: req.body.email,
       subject: `lagerbyggvest.no: Kontakt fra ${req.body.name}`,
       html: `<div>
